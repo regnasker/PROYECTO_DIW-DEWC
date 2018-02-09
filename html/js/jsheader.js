5e7b80aa68839046ea0952356417ba7bc4d0f465
nophyon <juantago.games@gmail.com>
2018-02-09 17:17:54 +0100

    Home responsive

--- html/js/jsheader.js
+++ html/js/jsheader.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-  $(".navegacion button").click(function () {
-    $(this).css("border-color","#45b5c6");
-    console.log(this.id);
-  });
 
+  var idButton = "nada";
+
+  $(".navegacion button").click(function () {
+    if (idButton != this.id) {
+      $("#"+idButton).css("border-color","#343434");
+      $(this).css("border-color","#45b5c6");
+      idButton = this.id;
+    }
+  });
 });
